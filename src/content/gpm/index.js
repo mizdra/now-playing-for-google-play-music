69743cdf9f2b7523caa2c4f8350746fad102b772
mizdra <pp.mizdra@gmail.com>
2019-06-15 00:28:06 +0900

    feat: support `playCount` variable

--- src/content/gpm/index.js
+++ src/content/gpm/index.js
@@ -3,9 +3,13 @@ async function share() {
   const title = document.querySelector('#currently-playing-title').textContent
   const artist = document.querySelector('#player-artist').textContent
   const album = document.querySelector('.player-album').textContent
+  const playCount =
+    document.querySelector(
+      '#queue-overlay .currently-playing [data-col="play-count"]',
+    ).textContent || '0'
   const { gpmTemplate, hashtags } = await getConfig()
 
-  const text = renderText(gpmTemplate, title, artist, album)
+  const text = renderText(gpmTemplate, title, artist, album, playCount)
   const url = renderURL(text, hashtags)
 
   window.open(url)
@@ -30,6 +34,23 @@ function insertShareButton() {
   wrapper.insertBefore(shareButton, queue)
 }
 
+// 初期状態では再生回数が埋め込まれている DOM がまだレンダリングされないので,
+// 一瞬だけ #queue-overlay を表示して強制的にレンダリングする
+function prerenderQueueOverlay() {
+  const $queue = document.querySelector('#queue') // キューを表示するボタン
+  const $queueOverlay = document.querySelector('#queue-overlay') // キュー
+  const clickEvent = document.createEvent('MouseEvents')
+  clickEvent.initEvent('click', true, false)
+
+  // #queue-overlay を不可視にしてから表示
+  $queueOverlay.style.display = 'none'
+  $queue.dispatchEvent(clickEvent)
+  requestAnimationFrame(() => {
+    $queue.dispatchEvent(clickEvent)
+    $queueOverlay.style.display = null
+  })
+}
+
 // プレイヤーが挿入されるまで待機する
 async function waitPlayerInserted() {
   // #material-player-right-wrapper がDOMに読み込まれたら, プレイヤーが挿入されたとして扱う
@@ -41,4 +62,7 @@ async function waitPlayerInserted() {
 // DOMContentLoaded の後もDOMが動的に挿入されていくので,
 // プレイヤー が挿入されるまでポーリングして, 挿入された
 // タイミングで insertShareButton を呼び出す.
-waitPlayerInserted().then(insertShareButton)
+waitPlayerInserted().then(() => {
+  insertShareButton()
+  prerenderQueueOverlay()
+})
