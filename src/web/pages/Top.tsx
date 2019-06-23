b307a6a301e0524c9d3bad1b0e66a23314061331
mizdra <pp.mizdra@gmail.com>
2019-06-23 14:41:57 +0900

    feat: improve Top page

--- src/web/pages/Top.tsx
+++ src/web/pages/Top.tsx
@@ -1,5 +1,76 @@
 import { h } from 'preact'
 
 export function Top() {
-  return <div>Top</div>
+  return (
+    <div>
+      <div class="welcome">
+        <img src="/img/logo.svg" alt="アプリのロゴ" />
+        <h1>#NowListening for Google Play Music</h1>
+      </div>
+      <div class="container">
+        <main>
+          <section>
+            <h2>What's this?</h2>
+            <p>
+              It's tools to share music playing on Google Play Music / Youtube
+              Music to SNS.
+            </p>
+
+            <h2>What platform supported?</h2>
+            <ul>
+              <li>
+                <a href="https://chrome.google.com/webstore/detail/nowplaying-for-google-pla/nhpanomgefidcljmcmkbanhoomaglmlk">
+                  Chrome for Desktop
+                </a>
+              </li>
+              <li>
+                <a href="https://addons.mozilla.org/ja/firefox/addon/nowplaying-for-google-pla/">
+                  Firefox for Desktop
+                </a>
+              </li>
+              <li>
+                <a href="/#how-to-install">Chrome for Android</a>
+              </li>
+            </ul>
+
+            <h2 name="how-to-install">
+              How do you install the version of Chrome for Android?
+            </h2>
+            <ol>
+              <li>Open this page with Chrome for Android</li>
+              <ol>
+                <li>
+                  Tap menu (
+                  <img src="/img/more_vert.svg" alt="Menu Icon" class="icon" />)
+                </li>
+                <li>
+                  Tap <code>Add to Home screen</code> ( Japanese:{' '}
+                  <code>ホーム画面に追加</code> )
+                </li>
+              </ol>
+              <li>Open Google Play Music App for Android</li>
+              <ol>
+                <li>Play your favorite music</li>
+                <li>
+                  Tap menu (
+                  <img src="/img/more_vert.svg" alt="Menu Icon" class="icon" />)
+                  in Google Play Music
+                </li>
+                <li>
+                  Select #NowPlaying (
+                  <img
+                    src="/img/logo.svg"
+                    alt="#NowPlaying Icon"
+                    class="icon"
+                  />
+                  )
+                </li>
+                <li>Let's Tweet!</li>
+              </ol>
+            </ol>
+          </section>
+        </main>
+      </div>
+    </div>
+  )
 }
